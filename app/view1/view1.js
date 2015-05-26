51e3fb12833df11b098839d79e197aaeaf18135e
Xavier Carol i Rosell <xcarol@xicra.com>
2015-05-26 18:35:06 +0200

    Work in progress...

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,21 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$rootScope', '$scope', '$http', '$location', '$log', function ($rootScope, $scope, $http, $location, $log) {
 
-}]);
+        $http.get('../series.json').success(function (data) {
+            $scope.series = data;
+        });
+
+        $scope.gotoEdit = function(serie){
+            $rootScope.serieToEdit = serie;
+            $location.path('/view2');
+        };
+    }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,16 +1,59 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.view1 module', function () {
 
-  beforeEach(module('myApp.view1'));
+    beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+    describe('view1 controller', function () {
+        var scope = {};
+        var ctrl;
+        var $httpBackend;
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('../series.json').respond([{
+                "name": "American Horror Story",
+                "season": 4,
+                "chapter": 13,
+                "enabled": "True",
+                "url": "http://www.divxtotal.com/series/american-horror-story-453/",
+                "xpath": "//td[@class=\"capitulodescarga\"]/a",
+                "xpath_link": "@href",
+                "xpath_title": "",
+                "link_prefix": "http://www.divxtotal.com",
+                "link_mask": "Story.#1x#2.HDTV",
+                "file_mask": "American.Horror.Story.#1x#2",
+                "title_mask": ""
+            }]);
 
-  });
+            scope = $rootScope.$new();
+            ctrl = $controller('View1Ctrl', {$scope: scope});
+        }));
+
+        it('should ....', inject(function ($controller) {
+            var view1Ctrl = $controller('View1Ctrl', {$scope: scope});
+            expect(view1Ctrl).toBeDefined();
+        }));
+
+        it('should create a serie model with all fields ...', inject(function(){
+            expect(scope.series).toBeUndefined();
+            $httpBackend.flush();
+            expect(scope.series.length).toBeGreaterThan(0);
+
+            var serie = scope.series[0];
+
+            expect(serie.chapter).toBeDefined();
+            expect(serie.xpath).toBeDefined();
+            expect(serie.name).toBeDefined();
+            expect(serie.url).toBeDefined();
+            expect(serie.season).toBeDefined();
+            expect(serie.xpath_link).toBeDefined();
+            expect(serie.enabled).toBeDefined();
+            expect(serie.link_prefix).toBeDefined();
+            expect(serie.file_mask).toBeDefined();
+            expect(serie.link_mask).toBeDefined();
+            expect(serie.xpath_title).toBeDefined();
+            expect(serie.title_mask).toBeDefined();
+        }));
+    });
 });
